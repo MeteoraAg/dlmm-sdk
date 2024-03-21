a469cfe72a33685a1c96d3d3b85285500982a0d7
namgold <namgold@raccoons.dev>
2024-03-21 16:51:07 +0700

    format prettier & update return type checkPoolExists

--- ts-client/src/dlmm/helpers/derive.ts
+++ ts-client/src/dlmm/helpers/derive.ts
@@ -1,59 +1,101 @@
-import { BN } from '@coral-xyz/anchor';
-import { Connection, PublicKey } from '@solana/web3.js';
+import { BN } from "@coral-xyz/anchor";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { DLMM } from "..";
 
 /** private */
 function sortTokenMints(tokenX: PublicKey, tokenY: PublicKey) {
-    const [minKey, maxKey] = tokenX.toBuffer().compare(tokenY.toBuffer()) == 1 ? [tokenY, tokenX] : [tokenX, tokenY];
-    return [minKey, maxKey];
+  const [minKey, maxKey] =
+    tokenX.toBuffer().compare(tokenY.toBuffer()) == 1
+      ? [tokenY, tokenX]
+      : [tokenX, tokenY];
+  return [minKey, maxKey];
 }
 /** private */
 
 export function derivePresetParameter(binStep: BN, programId: PublicKey) {
-    return PublicKey.findProgramAddressSync(
-        [
-            Buffer.from("preset_parameter"),
-            new Uint8Array(binStep.toBuffer("le", 2)),
-        ],
-        programId
-    );
+  return PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("preset_parameter"),
+      new Uint8Array(binStep.toBuffer("le", 2)),
+    ],
+    programId
+  );
 }
 
-export function deriveLbPair(tokenX: PublicKey, tokenY: PublicKey, binStep: BN, programId: PublicKey) {
-    const [minKey, maxKey] = sortTokenMints(tokenX, tokenY);
-    return PublicKey.findProgramAddressSync(
-        [minKey.toBuffer(), maxKey.toBuffer(), new Uint8Array(binStep.toArrayLike(Buffer, 'le', 2))],
-        programId,
-    );
+export function deriveLbPair(
+  tokenX: PublicKey,
+  tokenY: PublicKey,
+  binStep: BN,
+  programId: PublicKey
+) {
+  const [minKey, maxKey] = sortTokenMints(tokenX, tokenY);
+  return PublicKey.findProgramAddressSync(
+    [
+      minKey.toBuffer(),
+      maxKey.toBuffer(),
+      new Uint8Array(binStep.toArrayLike(Buffer, "le", 2)),
+    ],
+    programId
+  );
 }
 
-export async function checkPoolExists(connection: Connection, tokenX: PublicKey, tokenY: PublicKey, binStep: BN, programId: PublicKey) {
+export async function checkPoolExists(
+  connection: Connection,
+  tokenX: PublicKey,
+  tokenY: PublicKey,
+  binStep: BN,
+  programId: PublicKey
+): Promise<PublicKey | false> {
+  try {
     const [lbPairKey] = deriveLbPair(tokenX, tokenY, binStep, programId);
-    const dlmm = await DLMM.create(connection,lbPairKey);
-    const { activeId } = await dlmm.program.account.lbPair.fetch(lbPairKey);
-    return !!activeId
+    const dlmm = await DLMM.create(connection, lbPairKey);
+    await dlmm.program.account.lbPair.fetch(lbPairKey);
+    return lbPairKey;
+  } catch {
+    return false;
+  }
 }
 
 export function deriveOracle(lbPair: PublicKey, programId: PublicKey) {
-    return PublicKey.findProgramAddressSync([Buffer.from('oracle'), lbPair.toBytes()], programId);
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("oracle"), lbPair.toBytes()],
+    programId
+  );
 }
 
 export function derivePosition(mint: PublicKey, programId: PublicKey) {
-    return PublicKey.findProgramAddressSync([Buffer.from('position'), mint.toBuffer()], programId);
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("position"), mint.toBuffer()],
+    programId
+  );
 }
 
-
-
-export function deriveBinArray(lbPair: PublicKey, index: BN, programId: PublicKey) {
-    let binArrayBytes: Uint8Array;
-    if (index.isNeg()) {
-        binArrayBytes = new Uint8Array(index.toTwos(64).toBuffer('le', 8));
-    } else {
-        binArrayBytes = new Uint8Array(index.toBuffer('le', 8));
-    }
-    return PublicKey.findProgramAddressSync([Buffer.from('bin_array'), lbPair.toBytes(), binArrayBytes], programId);
+export function deriveBinArray(
+  lbPair: PublicKey,
+  index: BN,
+  programId: PublicKey
+) {
+  let binArrayBytes: Uint8Array;
+  if (index.isNeg()) {
+    binArrayBytes = new Uint8Array(
+      index.toTwos(64).toArrayLike(Buffer, "le", 8)
+    );
+  } else {
+    binArrayBytes = new Uint8Array(index.toArrayLike(Buffer, "le", 8));
+  }
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("bin_array"), lbPair.toBytes(), binArrayBytes],
+    programId
+  );
 }
 
-export function deriveReserve(token: PublicKey, lbPair: PublicKey, programId: PublicKey) {
-    return PublicKey.findProgramAddressSync([lbPair.toBuffer(), token.toBuffer()], programId);
+export function deriveReserve(
+  token: PublicKey,
+  lbPair: PublicKey,
+  programId: PublicKey
+) {
+  return PublicKey.findProgramAddressSync(
+    [lbPair.toBuffer(), token.toBuffer()],
+    programId
+  );
 }
