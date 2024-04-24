847a1bb3d820818eee6a461f5ea3f0ad29be040a
codewithgun <codewithgun@gmail.com>
2024-04-24 14:46:54 +0800

    add more test for ilm seed liq

--- ts-client/src/test/ilm.test.ts
+++ ts-client/src/test/ilm.test.ts
@@ -281,4 +281,239 @@ describe("ILM test", () => {
       console.log(babar(binLiquidities));
     });
   });
+  describe("Shaky", () => {
+    const baseKeypair = Keypair.generate();
+    const sharkyDecimal = 6;
+    const usdcDecimal = 6;
+    const feeBps = new BN(250);
+    const lockDurationInSlot = new BN(0);
+
+    let SHARKY: web3.PublicKey;
+    let USDC: web3.PublicKey;
+    let userSHAKY: web3.PublicKey;
+    let userUSDC: web3.PublicKey;
+    let pairKey: web3.PublicKey;
+    let pair: DLMM;
+
+    const toLamportMultiplier = new Decimal(
+      10 ** (sharkyDecimal - usdcDecimal)
+    );
+
+    const minPrice = 0.5;
+    const maxPrice = 1.62;
+    const binStep = 80;
+    const curvature = 1;
+    const seedAmount = new BN(5_000_000_000_000);
+
+    const minBinId = DLMM.getBinIdFromPrice(
+      new Decimal(minPrice).mul(toLamportMultiplier),
+      binStep,
+      false
+    );
+
+    beforeAll(async () => {
+      SHARKY = await createMint(
+        connection,
+        keypair,
+        keypair.publicKey,
+        null,
+        sharkyDecimal,
+        Keypair.generate(),
+        null,
+        TOKEN_PROGRAM_ID
+      );
+
+      USDC = await createMint(
+        connection,
+        keypair,
+        keypair.publicKey,
+        null,
+        usdcDecimal,
+        Keypair.generate(),
+        null,
+        TOKEN_PROGRAM_ID
+      );
+
+      const userShakyInfo = await getOrCreateAssociatedTokenAccount(
+        connection,
+        keypair,
+        SHARKY,
+        keypair.publicKey,
+        false,
+        "confirmed",
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      );
+      userSHAKY = userShakyInfo.address;
+
+      const userUsdcInfo = await getOrCreateAssociatedTokenAccount(
+        connection,
+        keypair,
+        USDC,
+        keypair.publicKey,
+        false,
+        "confirmed",
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      );
+      userUSDC = userUsdcInfo.address;
+
+      await mintTo(
+        connection,
+        keypair,
+        SHARKY,
+        userSHAKY,
+        keypair.publicKey,
+        200_000_000_000 * 10 ** sharkyDecimal,
+        [],
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID
+      );
+
+      await mintTo(
+        connection,
+        keypair,
+        USDC,
+        userUSDC,
+        keypair.publicKey,
+        1_000_000_000 * 10 ** usdcDecimal,
+        [],
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID
+      );
+
+      let rawTx = await DLMM.createPermissionLbPair(
+        connection,
+        new BN(binStep),
+        SHARKY,
+        USDC,
+        new BN(minBinId.toString()),
+        baseKeypair.publicKey,
+        keypair.publicKey,
+        feeBps,
+        lockDurationInSlot,
+        { cluster: "localhost" }
+      );
+      let txHash = await sendAndConfirmTransaction(connection, rawTx, [
+        keypair,
+        baseKeypair,
+      ]).catch((e) => {
+        console.error(e);
+        throw e;
+      });
+      console.log("Create permissioned LB pair", txHash);
+
+      [pairKey] = derivePermissionLbPair(
+        baseKeypair.publicKey,
+        SHARKY,
+        USDC,
+        new BN(binStep),
+        programId
+      );
+
+      pair = await DLMM.create(connection, pairKey, {
+        cluster: "localhost",
+      });
+
+      const walletToWhitelist = keypair.publicKey;
+      rawTx = await pair.updateWhitelistedWallet(walletToWhitelist);
+      txHash = await sendAndConfirmTransaction(connection, rawTx, [keypair]);
+      console.log("Update whitelisted wallet", txHash);
+      expect(txHash).not.toBeNull();
+    });
+
+    it("seed liquidity", async () => {
+      const groupedInstructions = await pair.seedLiquidity(
+        keypair.publicKey,
+        keypair.publicKey,
+        keypair.publicKey,
+        seedAmount,
+        curvature,
+        minPrice,
+        maxPrice,
+        baseKeypair.publicKey
+      );
+
+      const beforeTokenXBalance = await connection
+        .getTokenAccountBalance(userSHAKY)
+        .then((i) => new BN(i.value.amount));
+
+      for (const groupIx of groupedInstructions) {
+        const requireBaseSignature = groupIx.find((ix) =>
+          ix.keys.find((key) => key.pubkey.equals(baseKeypair.publicKey))
+        );
+
+        const { blockhash, lastValidBlockHeight } =
+          await connection.getLatestBlockhash("confirmed");
+
+        const tx = new Transaction({
+          feePayer: keypair.publicKey,
+          blockhash,
+          lastValidBlockHeight,
+        }).add(...groupIx);
+
+        const signers = [keypair];
+
+        if (requireBaseSignature) {
+          signers.push(baseKeypair);
+        }
+
+        const txHash = await sendAndConfirmTransaction(
+          connection,
+          tx,
+          signers
+        ).catch((e) => {
+          console.error(e);
+          throw e;
+        });
+        console.log(txHash);
+      }
+
+      const afterTokenXBalance = await connection
+        .getTokenAccountBalance(userSHAKY)
+        .then((i) => new BN(i.value.amount));
+
+      const actualDepositedAmount = beforeTokenXBalance.sub(afterTokenXBalance);
+      expect(actualDepositedAmount.toString()).toEqual(seedAmount.toString());
+
+      let binArrays = await pair.getBinArrays();
+      binArrays = binArrays.sort((a, b) =>
+        a.account.index.cmp(b.account.index)
+      );
+
+      const binLiquidities = binArrays
+        .map((ba) => {
+          const [lowerBinId, upperBinId] = getBinArrayLowerUpperBinId(
+            ba.account.index
+          );
+          const binWithLiquidity: [number, number][] = [];
+          for (let i = lowerBinId.toNumber(); i <= upperBinId.toNumber(); i++) {
+            const binAmountX =
+              ba.account.bins[i - lowerBinId.toNumber()].amountX;
+            const binPrice = getPriceOfBinByBinId(i, pair.lbPair.binStep);
+            const liquidity = new Decimal(binAmountX.toString())
+              .mul(binPrice)
+              .floor()
+              .toNumber();
+            binWithLiquidity.push([i, liquidity]);
+          }
+          return binWithLiquidity;
+        })
+        .flat();
+
+      console.log(binLiquidities.filter((b) => b[1] > 0).reverse());
+      console.log(binLiquidities.filter((b) => b[1] > 0));
+      console.log(babar(binLiquidities));
+    });
+  });
 });
