5b90ccb834f552aa98dd89e116129d99ac143368
codewithgun <codewithgun@gmail.com>
2024-06-06 09:33:49 +0800

    doc: update changelog, and add comments

--- commons/src/quote.rs
+++ commons/src/quote.rs
@@ -164,6 +164,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_swap_quote_exact_in() {
+        // RPC client. No gPA is required.
         let rpc_client = RpcClient::new(Cluster::Mainnet.url().to_string());
 
         let client = Client::new(
@@ -177,12 +178,16 @@ mod tests {
 
         let lb_pair = program.account::<LbPair>(SOL_USDC).await.unwrap();
 
+        // 3 bin arrays to left, and right is enough to cover most of the swap, and stay under 1.4m CU constraint.
+        // Get 3 bin arrays to the left from the active bin
         let left_bin_array_pubkeys =
             get_bin_array_pubkeys_for_swap(SOL_USDC, &lb_pair, None, true, 3).unwrap();
 
+        // Get 3 bin arrays to the right the from active bin
         let right_bin_array_pubkeys =
             get_bin_array_pubkeys_for_swap(SOL_USDC, &lb_pair, None, false, 3).unwrap();
 
+        // Fetch bin arrays
         let bin_array_pubkeys = left_bin_array_pubkeys
             .into_iter()
             .chain(right_bin_array_pubkeys.into_iter())
@@ -204,6 +209,7 @@ mod tests {
             })
             .collect::<HashMap<_, _>>();
 
+        // 1 SOL -> USDC
         let in_sol_amount = 1_000_000_000;
 
         let quote_result = quote_exact_in(
@@ -221,6 +227,7 @@ mod tests {
             quote_result.amount_out as f64 / 1_000_000.0
         );
 
+        // 100 USDC -> SOL
         let in_usdc_amount = 100_000_000;
 
         let quote_result = quote_exact_in(
