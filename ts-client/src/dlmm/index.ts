e371f41b793fe0aabbde08bef7bc94bfbc313af8
Max Koverdyaev <shadeglare@gmail.com>
2024-06-24 16:57:25 +0300

    fix empty reward claims

--- ts-client/src/dlmm/index.ts
+++ ts-client/src/dlmm/index.ts
@@ -4034,6 +4034,10 @@ export class DLMM {
       }
     }
 
+    // Filter only position with fees and/or rewards
+    positions = positions.filter(({ positionData: { feeX, feeY, rewardOne, rewardTwo } }) =>
+      !feeX.isZero() || !feeY.isZero() || !rewardOne.isZero() || !rewardTwo.isZero());
+
     const feeOwners = [
       ...new Set([
         owner.toBase58(),
