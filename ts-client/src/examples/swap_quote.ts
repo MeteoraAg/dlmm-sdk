afd104dcf8bc33bbb3843a0d2f951c60387613e7
codewithgun <codewithgun@gmail.com>
2024-07-26 20:54:18 +0800

    feat: expose initialize bin array and helper to get bin arrays

--- ts-client/src/examples/swap_quote.ts
+++ ts-client/src/examples/swap_quote.ts
@@ -1,11 +1,13 @@
-import {
-  Connection,
-  PublicKey,
-} from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { DLMM } from "../dlmm";
 import BN from "bn.js";
 
-async function swapQuote(poolAddress: PublicKey, swapAmount: BN, swapYtoX: boolean, isPartialFill: boolean) {
+async function swapQuote(
+  poolAddress: PublicKey,
+  swapAmount: BN,
+  swapYtoX: boolean,
+  isPartialFill: boolean
+) {
   let rpc = "https://api.mainnet-beta.solana.com";
   const connection = new Connection(rpc, "finalized");
   const dlmmPool = await DLMM.create(connection, poolAddress, {
@@ -13,16 +15,28 @@ async function swapQuote(poolAddress: PublicKey, swapAmount: BN, swapYtoX: boole
   });
 
   const binArrays = await dlmmPool.getBinArrayForSwap(swapYtoX);
-  const swapQuote = await dlmmPool.swapQuote(swapAmount, swapYtoX, new BN(10), binArrays, isPartialFill);
+  const swapQuote = await dlmmPool.swapQuote(
+    swapAmount,
+    swapYtoX,
+    new BN(10),
+    binArrays,
+    isPartialFill
+  );
   console.log("🚀 ~ swapQuote:", swapQuote);
-  console.log("consumedInAmount: %s, outAmount: %s", swapQuote.consumedInAmount.toString(), swapQuote.outAmount.toString());
+  console.log(
+    "consumedInAmount: %s, outAmount: %s",
+    swapQuote.consumedInAmount.toString(),
+    swapQuote.outAmount.toString()
+  );
 }
 
 async function main() {
-  await swapQuote(new PublicKey(
-    "8kCbYxnF8ggdJACxz4NLYtVhEEz6EBvN5NQcKAazpkEY"
-  ), new BN(1_000_000_000), true, true);
+  await swapQuote(
+    new PublicKey("8kCbYxnF8ggdJACxz4NLYtVhEEz6EBvN5NQcKAazpkEY"),
+    new BN(1_000_000_000),
+    true,
+    true
+  );
 }
 
-
-main()
+main();
