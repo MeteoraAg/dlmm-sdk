cffecd7d9306cf5199eb6a1843bdfd169dc6479b
quangkeu95 <quangkeu95@gmail.com>
2024-12-11 22:38:47 +0700

    chore: Update test single bin

--- ts-client/src/test/single_bin.test.ts
+++ ts-client/src/test/single_bin.test.ts
@@ -34,3 +34,109 @@ const keypair = Keypair.fromSecretKey(
   new Uint8Array(JSON.parse(keypairBuffer))
 );
 const programId = new PublicKey(LBCLMM_PROGRAM_IDS["localhost"]);
+
+describe("Single Bin Seed Liquidity Test", () => {
+  describe("TokenX decimals < TokenY decimals", () => {
+    const baseKeypair = Keypair.generate();
+    const wenDecimal = 5;
+    const usdcDecimal = 6;
+    const feeBps = new BN(500);
+    const initialPrice = 0.000001;
+    const binStep = 100;
+    const seedAmount = new BN(200_000_000_000);
+
+    let WEN: web3.PublicKey;
+    let USDC: web3.PublicKey;
+    let userWEN: web3.PublicKey;
+    let userUSDC: web3.PublicKey;
+    let pairKey: web3.PublicKey;
+    let pair: DLMM;
+
+    const initialPricePerLamport = DLMM.getPricePerLamport(wenDecimal, usdcDecimal, initialPrice);
+    const binId = DLMM.getBinIdFromPrice(initialPricePerLamport, binStep, false);
+
+    beforeAll(async () => {
+      WEN = await createMint(
+        connection,
+        keypair,
+        keypair.publicKey,
+        null,
+        wenDecimal,
+        Keypair.generate(),
+        null,
+        TOKEN_PROGRAM_ID
+      );
+
+      USDC = await createMint(
+        connection,
+        keypair,
+        keypair.publicKey,
+        null,
+        usdcDecimal,
+        Keypair.generate(),
+        null,
+        TOKEN_PROGRAM_ID
+      );
+
+      const userWenInfo = await getOrCreateAssociatedTokenAccount(
+        connection,
+        keypair,
+        WEN,
+        keypair.publicKey,
+        false,
+        "confirmed",
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      );
+      userWEN = userWenInfo.address;
+
+      const userUsdcInfo = await getOrCreateAssociatedTokenAccount(
+        connection,
+        keypair,
+        USDC,
+        keypair.publicKey,
+        false,
+        "confirmed",
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      );
+      userUSDC = userUsdcInfo.address;
+
+      await mintTo(
+        connection,
+        keypair,
+        WEN,
+        userWEN,
+        keypair.publicKey,
+        200_000_000_000 * 10 ** wenDecimal,
+        [],
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID
+      );
+
+      await mintTo(
+        connection,
+        keypair,
+        USDC,
+        userUSDC,
+        keypair.publicKey,
+        1_000_000_000 * 10 ** usdcDecimal,
+        [],
+        {
+          commitment: "confirmed",
+        },
+        TOKEN_PROGRAM_ID
+      );
+
+    });
+
+  })
+})
