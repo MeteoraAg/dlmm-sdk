40fdf9717699a7eb7be192be4d0380d6aec8036a
codewithgun <codewithgun@gmail.com>
2024-12-12 19:12:43 +0800

    chore: remove unused cu constant

--- ts-client/src/dlmm/helpers/computeUnit.ts
+++ ts-client/src/dlmm/helpers/computeUnit.ts
@@ -2,7 +2,3 @@
 export const DEFAULT_ADD_LIQUIDITY_CU = 800_000;
 // https://solscan.io/tx/49AGqJLki7DWcXWU9xdvLDmtTJFjfhpEUYKBbMjkDmjqQ9xJfvkrqTuSKX9Ac5cewsnPp9X818WsskzscwU72b4i
 export const DEFAULT_REMOVE_LIQUIDITY_CU = 600_000;
-// https://solscan.io/tx/3FGmkv9vpmdePVqm37A9ULhNvhZKFuKysVeXjqHAFNJvRczvoJp6c1rpiF2SwUQUNsQ25rhR81Az6wbZqb7Dhv6y
-export const DEFAULT_CLAIM_FEE_CU = 400_000;
-export const DEFAULT_CLAIM_REWARD_CU = 400_000;
-export const DEFAULT_COMBINED_CLAIM_FEE_AND_REWARD_CU = 600_000;
