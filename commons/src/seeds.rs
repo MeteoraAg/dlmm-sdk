0b1570ce2af16b6753941f4318968fe1678fb00b
codewithgun <codewithgun@gmail.com>
2024-12-19 17:27:19 +0800

    refactor: fix rust swap quote and test after switch to dlmm interface

---
+++ commons/src/seeds.rs
@@ -0,0 +1,14 @@
+use solana_sdk::pubkey;
+use solana_sdk::pubkey::Pubkey;
+
+pub const BIN_ARRAY: &[u8] = b"bin_array";
+
+pub const ORACLE: &[u8] = b"oracle";
+
+pub const BIN_ARRAY_BITMAP_SEED: &[u8] = b"bitmap";
+
+pub const PRESET_PARAMETER: &[u8] = b"preset_parameter";
+
+pub const POSITION: &[u8] = b"position";
+
+pub const ILM_BASE_KEY: Pubkey = pubkey!("MFGQxwAmB91SwuYX36okv2Qmdc9aMuHTwWGUrp4AtB1");
