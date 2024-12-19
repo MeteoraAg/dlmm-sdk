fe8bddcafa90d107fe530b0ed675e0bf20fecbd2
codewithgun <codewithgun@gmail.com>
2024-12-19 12:01:00 +0800

    refactor: prepare dlmm_interface to replace program interface

---
+++ dlmm_interface/src/lib.rs
@@ -0,0 +1,11 @@
+solana_program::declare_id!("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo");
+pub mod accounts;
+pub use accounts::*;
+pub mod typedefs;
+pub use typedefs::*;
+pub mod instructions;
+pub use instructions::*;
+pub mod errors;
+pub use errors::*;
+pub mod events;
+pub use events::*;
