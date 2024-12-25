4a41fac3a9b6ec380a12a35185887d3d181ef3de
codewithgun <codewithgun@gmail.com>
2024-12-25 13:06:48 +0800

    fix: some cli todos

--- commons/src/extensions/position_v3.rs
+++ commons/src/extensions/position_v3.rs
@@ -5,18 +5,53 @@ pub trait PositionV3Extension {
     fn get_bin_array_indexes_coverage(&self) -> Result<(i32, i32)>;
     fn get_bin_array_keys_coverage(&self) -> Result<Vec<Pubkey>>;
     fn get_bin_array_accounts_meta_coverage(&self) -> Result<Vec<AccountMeta>>;
+
+    fn get_bin_array_indexes_coverage_by_chunk(
+        &self,
+        lower_bin_id: i32,
+        upper_bin_id: i32,
+    ) -> Result<(i32, i32)>;
+
+    fn get_bin_array_keys_coverage_by_chunk(
+        &self,
+        lower_bin_id: i32,
+        upper_bin_id: i32,
+    ) -> Result<Vec<Pubkey>>;
+
+    fn get_bin_array_accounts_meta_coverage_by_chunk(
+        &self,
+        lower_bin_id: i32,
+        upper_bin_id: i32,
+    ) -> Result<Vec<AccountMeta>>;
 }
 
 impl PositionV3Extension for PositionV3 {
     fn get_bin_array_indexes_coverage(&self) -> Result<(i32, i32)> {
-        let lower_bin_array_index = BinArray::bin_id_to_bin_array_index(self.lower_bin_id)?;
-        let upper_bin_array_index = BinArray::bin_id_to_bin_array_index(self.upper_bin_id)?;
+        self.get_bin_array_indexes_coverage_by_chunk(self.lower_bin_id, self.upper_bin_id)
+    }
+
+    fn get_bin_array_indexes_coverage_by_chunk(
+        &self,
+        lower_bin_id: i32,
+        upper_bin_id: i32,
+    ) -> Result<(i32, i32)> {
+        ensure!(lower_bin_id >= self.lower_bin_id && upper_bin_id <= self.upper_bin_id);
+        let lower_bin_array_index = BinArray::bin_id_to_bin_array_index(lower_bin_id)?;
+        let upper_bin_array_index = BinArray::bin_id_to_bin_array_index(upper_bin_id)?;
         Ok((lower_bin_array_index, upper_bin_array_index))
     }
 
     fn get_bin_array_keys_coverage(&self) -> Result<Vec<Pubkey>> {
+        self.get_bin_array_keys_coverage_by_chunk(self.lower_bin_id, self.upper_bin_id)
+    }
+
+    fn get_bin_array_keys_coverage_by_chunk(
+        &self,
+        lower_bin_id: i32,
+        upper_bin_id: i32,
+    ) -> Result<Vec<Pubkey>> {
         let (lower_bin_array_index, upper_bin_array_index) =
-            self.get_bin_array_indexes_coverage()?;
+            self.get_bin_array_indexes_coverage_by_chunk(lower_bin_id, upper_bin_id)?;
         let mut bin_array_keys = Vec::new();
         for bin_array_index in lower_bin_array_index..=upper_bin_array_index {
             bin_array_keys.push(derive_bin_array_pda(self.lb_pair, bin_array_index.into()).0);
@@ -25,7 +60,16 @@ impl PositionV3Extension for PositionV3 {
     }
 
     fn get_bin_array_accounts_meta_coverage(&self) -> Result<Vec<AccountMeta>> {
-        let bin_array_keys = self.get_bin_array_keys_coverage()?;
+        self.get_bin_array_accounts_meta_coverage_by_chunk(self.lower_bin_id, self.upper_bin_id)
+    }
+
+    fn get_bin_array_accounts_meta_coverage_by_chunk(
+        &self,
+        lower_bin_id: i32,
+        upper_bin_id: i32,
+    ) -> Result<Vec<AccountMeta>> {
+        let bin_array_keys =
+            self.get_bin_array_keys_coverage_by_chunk(lower_bin_id, upper_bin_id)?;
         Ok(bin_array_keys
             .into_iter()
             .map(|key| AccountMeta::new(key, false))
