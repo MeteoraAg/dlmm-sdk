0468d02ede516372c591891ea35eb02530c0566f
codewithgun <codewithgun@gmail.com>
2024-12-25 13:15:29 +0800

    refactor: cli structure

--- cli/src/instructions/mod.rs
+++ cli/src/instructions/mod.rs
@@ -10,9 +10,6 @@ pub use claim_reward::*;
 pub mod close_position;
 pub use close_position::*;
 
-pub mod close_preset_parameter;
-pub use close_preset_parameter::*;
-
 pub mod fund_reward;
 pub use fund_reward::*;
 
@@ -34,50 +31,17 @@ pub use initialize_customizable_permissionless_lb_pair::*;
 pub mod initialize_lb_pair;
 pub use initialize_lb_pair::*;
 
-pub mod initialize_permission_lb_pair;
-pub use initialize_permission_lb_pair::*;
-
 pub mod initialize_position;
 pub use initialize_position::*;
 
 pub mod initialize_position_with_price_range;
 pub use initialize_position_with_price_range::*;
 
-pub mod initialize_preset_parameter;
-pub use initialize_preset_parameter::*;
-
-pub mod initialize_reward;
-pub use initialize_reward::*;
-
 pub mod list_all_binstep;
 pub use list_all_binstep::*;
 
 pub mod remove_liquidity;
 pub use remove_liquidity::*;
-
-pub mod remove_liquidity_by_price_range;
-pub use remove_liquidity_by_price_range::*;
-
-pub mod seed_liquidity;
-pub use seed_liquidity::*;
-
-pub mod seed_liquidity_from_operator;
-pub use seed_liquidity_from_operator::*;
-
-pub mod seed_liquidity_single_bin;
-pub use seed_liquidity_single_bin::*;
-
-pub mod seed_liquidity_single_bin_by_operator;
-pub use seed_liquidity_single_bin_by_operator::*;
-
-pub mod set_activation_point;
-pub use set_activation_point::*;
-
-pub mod set_pre_activation_duration;
-pub use set_pre_activation_duration::*;
-
-pub mod set_pre_activation_swap_address;
-pub use set_pre_activation_swap_address::*;
 
 pub mod show_pair;
 pub use show_pair::*;
@@ -91,23 +55,17 @@ pub use swap_exact_out::*;
 pub mod swap_with_price_impact;
 pub use swap_with_price_impact::*;
 
-pub mod toggle_pair_status;
-pub use toggle_pair_status::*;
-
-pub mod update_reward_duration;
-pub use update_reward_duration::*;
-
-pub mod update_reward_funder;
-pub use update_reward_funder::*;
-
 mod utils;
 pub use utils::*;
-
-pub mod withdraw_protocol_fee;
-pub use withdraw_protocol_fee::*;
 
 pub mod show_position;
 pub use show_position::*;
 
 pub mod show_preset_parameters;
 pub use show_preset_parameters::*;
+
+pub mod admin;
+pub use admin::*;
+
+pub mod ilm;
+pub use ilm::*;
