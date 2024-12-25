d44991ad0379ab4a5b94acbc6674fc2ef2804dac
codewithgun <codewithgun@gmail.com>
2024-12-25 13:15:29 +0800

    refactor: cli structure

---
+++ cli/src/instructions/admin/mod.rs
@@ -0,0 +1,32 @@
+pub mod close_preset_parameter;
+pub use close_preset_parameter::*;
+
+pub mod initialize_permission_lb_pair;
+pub use initialize_permission_lb_pair::*;
+
+pub mod initialize_preset_parameter;
+pub use initialize_preset_parameter::*;
+
+pub mod initialize_reward;
+pub use initialize_reward::*;
+
+pub mod set_activation_point;
+pub use set_activation_point::*;
+
+pub mod set_pre_activation_duration;
+pub use set_pre_activation_duration::*;
+
+pub mod set_pre_activation_swap_address;
+pub use set_pre_activation_swap_address::*;
+
+pub mod toggle_pair_status;
+pub use toggle_pair_status::*;
+
+pub mod update_reward_duration;
+pub use update_reward_duration::*;
+
+pub mod update_reward_funder;
+pub use update_reward_funder::*;
+
+pub mod withdraw_protocol_fee;
+pub use withdraw_protocol_fee::*;
