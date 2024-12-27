7c7085766f7ef857c07f239e1fd633b02bd9bbdb
codewithgun <codewithgun@gmail.com>
2024-12-27 19:51:35 +0800

    fix: token 2022 transfer fee

--- ts-client/src/dlmm/helpers/token_2022.ts
+++ ts-client/src/dlmm/helpers/token_2022.ts
@@ -1,8 +1,15 @@
 import {
   addExtraAccountMetasForExecute,
+  calculateFee,
   createTransferCheckedInstruction,
+  getEpochFee,
+  getTransferFeeConfig,
   getTransferHook,
+  MAX_FEE_BASIS_POINTS,
+  Mint,
+  TOKEN_2022_PROGRAM_ID,
   TOKEN_PROGRAM_ID,
+  TransferFee,
   unpackMint,
 } from "@solana/spl-token";
 import {
@@ -11,6 +18,7 @@ import {
   Connection,
   PublicKey,
 } from "@solana/web3.js";
+import BN from "bn.js";
 
 export async function getMultipleMintsExtraAccountMetasForTransferHook(
   connection: Connection,
@@ -46,6 +54,12 @@ export async function getExtraAccountMetasForTransferHook(
   mintAddress: PublicKey,
   mintAccountInfo: AccountInfo<Buffer>
 ) {
+  if (
+    ![TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID].includes(mintAccountInfo.owner)
+  ) {
+    return [];
+  }
+
   const mintState = unpackMint(
     mintAddress,
     mintAccountInfo,
@@ -87,4 +101,122 @@ export async function getExtraAccountMetasForTransferHook(
     // Only 4 keys needed if it's single signer. https://github.com/solana-labs/solana-program-library/blob/d72289c79a04411c69a8bf1054f7156b6196f9b3/token/js/src/extensions/transferFee/instructions.ts#L251
     return instruction.keys.slice(4);
   }
+}
+
+function calculatePreFeeAmount(transferFee: TransferFee, postFeeAmount: BN) {
+  if (postFeeAmount.isZero()) {
+    return new BN(0);
+  }
+
+  if (transferFee.transferFeeBasisPoints === 0) {
+    return postFeeAmount;
+  }
+
+  const maximumFee = new BN(transferFee.maximumFee.toString());
+
+  if (transferFee.transferFeeBasisPoints === MAX_FEE_BASIS_POINTS) {
+    return postFeeAmount.add(maximumFee);
+  }
+
+  const ONE_IN_BASIS_POINTS = new BN(MAX_FEE_BASIS_POINTS);
+  const numerator = postFeeAmount.mul(ONE_IN_BASIS_POINTS);
+  const denominator = ONE_IN_BASIS_POINTS.sub(
+    new BN(transferFee.transferFeeBasisPoints)
+  );
+
+  const rawPreFeeAmount = numerator
+    .add(ONE_IN_BASIS_POINTS)
+    .sub(new BN(1))
+    .div(denominator);
+
+  if (rawPreFeeAmount.sub(postFeeAmount).gte(maximumFee)) {
+    return postFeeAmount.add(maximumFee);
+  }
+
+  return rawPreFeeAmount;
+}
+
+function calculateInverseFee(transferFee: TransferFee, postFeeAmount: BN) {
+  const preFeeAmount = calculatePreFeeAmount(transferFee, postFeeAmount);
+  return new BN(
+    calculateFee(transferFee, BigInt(preFeeAmount.toString())).toString()
+  );
+}
+
+interface transferFeeIncludedAmount {
+  amount: BN;
+  transferFee: BN;
+}
+
+export function calculateTransferFeeIncludedAmount(
+  transferFeeExcludedAmount: BN,
+  mint: Mint,
+  currentEpoch: number
+): TransferFeeExcludedAmount {
+  if (transferFeeExcludedAmount.isZero()) {
+    return {
+      amount: new BN(0),
+      transferFee: new BN(0),
+    };
+  }
+
+  const transferFeeConfig = getTransferFeeConfig(mint);
+
+  if (transferFeeConfig === null) {
+    return {
+      amount: transferFeeExcludedAmount,
+      transferFee: new BN(0),
+    };
+  }
+
+  const epochFee = getEpochFee(transferFeeConfig, BigInt(currentEpoch));
+
+  const transferFee =
+    epochFee.transferFeeBasisPoints == MAX_FEE_BASIS_POINTS
+      ? new BN(epochFee.maximumFee.toString())
+      : calculateInverseFee(epochFee, transferFeeExcludedAmount);
+
+  const transferFeeIncludedAmount = transferFeeExcludedAmount.add(transferFee);
+
+  return {
+    amount: transferFeeIncludedAmount,
+    transferFee,
+  };
+}
+
+interface TransferFeeExcludedAmount {
+  amount: BN;
+  transferFee: BN;
+}
+
+export function calculateTransferFeeExcludedAmount(
+  transferFeeIncludedAmount: BN,
+  mint: Mint,
+  currentEpoch: number
+): TransferFeeExcludedAmount {
+  const transferFeeConfig = getTransferFeeConfig(mint);
+  if (transferFeeConfig === null) {
+    return {
+      amount: transferFeeIncludedAmount,
+      transferFee: new BN(0),
+    };
+  }
+
+  const transferFeeIncludedAmountN = BigInt(
+    transferFeeIncludedAmount.toString()
+  );
+
+  const transferFee = calculateFee(
+    getEpochFee(transferFeeConfig, BigInt(currentEpoch)),
+    transferFeeIncludedAmountN
+  );
+
+  const transferFeeExcludedAmount = new BN(
+    (transferFeeIncludedAmountN - transferFee).toString()
+  );
+
+  return {
+    amount: transferFeeExcludedAmount,
+    transferFee: new BN(transferFee.toString()),
+  };
 }
