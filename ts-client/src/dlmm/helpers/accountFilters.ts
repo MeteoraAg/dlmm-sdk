20d5b46d86a741644c77bec5d61d278f4102b4cd
codewithgun <codewithgun@gmail.com>
2024-12-28 17:44:58 +0800

    fix: position fee and reward quotes

--- ts-client/src/dlmm/helpers/accountFilters.ts
+++ ts-client/src/dlmm/helpers/accountFilters.ts
@@ -1,6 +1,40 @@
 import { GetProgramAccountsFilter, PublicKey } from "@solana/web3.js";
 import { POSITION_V3_DISC } from "../types";
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import BN from "bn.js";
+
+export const presetParameter2BinStepFilter = (
+  binStep: BN
+): GetProgramAccountsFilter => {
+  return {
+    memcmp: {
+      bytes: bs58.encode(binStep.toArrayLike(Buffer, "le", 2)),
+      offset: 8,
+    },
+  };
+};
+
+export const presetParameter2BaseFactorFilter = (
+  baseFactor: BN
+): GetProgramAccountsFilter => {
+  return {
+    memcmp: {
+      bytes: bs58.encode(baseFactor.toArrayLike(Buffer, "le", 2)),
+      offset: 8 + 2,
+    },
+  };
+};
+
+export const presetParameter2BaseFeePowerFactor = (
+  baseFeePowerFactor: BN
+): GetProgramAccountsFilter => {
+  return {
+    memcmp: {
+      bytes: bs58.encode(baseFeePowerFactor.toArrayLike(Buffer, "le", 1)),
+      offset: 8 + 22,
+    },
+  };
+};
 
 export const binArrayLbPairFilter = (
   lbPair: PublicKey
