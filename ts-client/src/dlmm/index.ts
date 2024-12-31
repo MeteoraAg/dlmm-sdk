4d3dbd6b75d9ac38b4ae25444edfedccf51405cb
codewithgun <codewithgun@gmail.com>
2024-12-31 14:01:55 +0800

    fix: ts test

--- ts-client/src/dlmm/index.ts
+++ ts-client/src/dlmm/index.ts
@@ -920,7 +920,7 @@ export class DLMM {
     >();
 
     lbPairKeys.forEach((lbPair, idx) => {
-      const index = idx * 4;
+      const index = idx * 6;
       const reserveXAccount = accountInfos[index];
       const reserveYAccount = accountInfos[index + 1];
 
@@ -2897,6 +2897,7 @@ export class DLMM {
       sender: user,
       tokenXProgram: this.tokenX.owner,
       tokenYProgram: this.tokenY.owner,
+      memoProgram: MEMO_PROGRAM_ID,
     };
 
     const { slices, accounts: transferHookAccounts } =
@@ -2939,7 +2940,7 @@ export class DLMM {
   }
 
   /**
-   * @deprecated Use `addLiquidityByStrategy`
+   * @deprecated Do not support dynamic position. Use `addLiquidityByStrategy`
    * The `addLiquidityByWeight` function is used to add liquidity to existing position
    * @param {TInitializePositionAndAddLiquidityParams}
    *    - `positionPubKey`: The public key of the position account. (usually use `new Keypair()`)
@@ -3218,7 +3219,8 @@ export class DLMM {
    * @param
    *    - `user`: The public key of the user account.
    *    - `position`: The public key of the position account.
-   *    - `binIds`: An array of numbers that represent the bin IDs to remove liquidity from.
+   *    - `fromBinId`: The ID of the starting bin to remove liquidity from. Must within position range.
+   *    - `toBinId`: The ID of the ending bin to remove liquidity from. Must within position range.
    *    - `liquiditiesBpsToRemove`: An array of numbers (percentage) that represent the liquidity to remove from each bin.
    *    - `shouldClaimAndClose`: A boolean flag that indicates whether to claim rewards and close the position.
    * @returns {Promise<Transaction|Transaction[]>}
@@ -3226,18 +3228,20 @@ export class DLMM {
   public async removeLiquidity({
     user,
     position,
-    binIds,
+    fromBinId,
+    toBinId,
     bps,
     shouldClaimAndClose = false,
   }: {
     user: PublicKey;
     position: PublicKey;
-    binIds: number[];
+    fromBinId: number;
+    toBinId: number;
     bps: BN;
     shouldClaimAndClose?: boolean;
   }): Promise<Transaction | Transaction[]> {
-    const lowerBinIdToRemove = Math.min(...binIds);
-    const upperBinIdToRemove = Math.max(...binIds);
+    const lowerBinIdToRemove = fromBinId;
+    const upperBinIdToRemove = toBinId;
 
     const positionAccount =
       await this.program.provider.connection.getAccountInfo(position);
@@ -3627,6 +3631,7 @@ export class DLMM {
     const resizePositionIx = await this.program.methods
       .increasePositionLength(lengthToAdd.toNumber(), side)
       .accounts({
+        lbPair: this.pubkey,
         funder: payer,
         position,
         owner: positionState.owner,
