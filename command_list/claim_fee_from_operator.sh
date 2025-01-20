fee0cfb6fbc7f6875ef77c04f44e36eb7fd1f820
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-01-21 03:20:24 +0700

    hotfix cli

--- command_list/claim_fee_from_operator.sh
+++ command_list/claim_fee_from_operator.sh
@@ -1,13 +1,12 @@
 cluster=https://api.mainnet-beta.solana.com
-lb_pair=[Pool pk]
-owner=[Owner pk]
-
 operator_kp=~/.config/solana/id.json
 priority_fee=1000
 
-# get all positions
-# ./target/debug/cli --provider.cluster $cluster get-all-positions-for-an-owner --lb-pair $lb_pair --owner $owner
-
-# claim fee
 position=[Position pk]
 ./target/debug/cli --provider.cluster $cluster --provider.wallet $operator_kp --priority-fee $priority_fee claim-fee $position
+
+# get all positions
+# lb_pair=[Pool pk]
+# owner=[Owner pk]
+# ./target/debug/cli --provider.cluster $cluster get-all-positions-for-an-owner --lb-pair $lb_pair --owner $owner
+
