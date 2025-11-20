3d32c991c2a76b6433eb6b8fd238ddb99cb80ce0
Dann Wee <dannweezj@gmail.com>
2025-11-20 14:41:49 +0800

    fix: modern esm transpile error

--- ts-client/tsup.config.ts
+++ ts-client/tsup.config.ts
@@ -3,13 +3,14 @@ import type { Options } from "tsup";
 const config: Options = {
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   minify: false,
   clean: true,
   skipNodeModulesBundle: true,
   dts: true,
   external: ["node_modules"],
+  target: "es2020",
   // post-process CJS build to fix default export for require() users
   onSuccess: async () => {
     const fs = await import("fs");
